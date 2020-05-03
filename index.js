94ea1a34aa704ef55f81590fcec43248abbc961c
wachaon <wachaon@gmail.com>
2020-05-03 22:48:35 +0900

    Update prettier to 2.0.5

--- index.js
+++
@@ -1,7 +0,0 @@
-const prettier = require( '/node_modules/prettier' )
-const babylon = require( '/node_modules/parser-babylon' )
-
-module.exports = {
-    prettier,
-    babylon
-}
--- usage.js
+++
@@ -1,3 +0,0 @@
-const { prettier, babylon } = require( '@wachaon/prettier' )
-
-console.log( prettier.format( 'var one = { a: 2, "one":1}', { parser: 'babel', plugins: [ babylon ] } ) )
