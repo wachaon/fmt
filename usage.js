5ad0fa3388d8628f02e54e2c32db7c686dba9eb0
wachaon <wachaon@gmail.com>
2020-05-06 09:23:32 +0900

    Will fix the bug

--- usage.js
+++ usage.js
@@ -1,7 +1,16 @@
-const { prettier, babylon } = require( '@wachaon/prettier' )
+/* test */
+const prettier = require('node_modules/prettier')
+const babel = require('node_modules/babel')
+
+
+//const { prettier, babel } = require( '@wachaon/prettier' )
+
+const log = require('log')
+// log(()=> prettier.format)
+
 const opt = {
     parser: 'babel',
-    plugins: [ babylon ]
+    plugins: [ babel ]
 }
 const source = 'function foo(  a,b ){return a +b}'
 console.log( prettier.format( source, opt ) )
