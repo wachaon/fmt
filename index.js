8cc59b6e1aee51d03cf0aa41553b08ef42b90c1f
wachaon <wachaon@gmail.com>
2020-05-28 00:01:45 +0900

    Will bundle again

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ let defaultOption = {
 }
 
 const prettierrc = resolve(process.cwd(), '.prettierrc')
-if (existsFileSync(prettierrc)) defaultOption = Object.assign(defaultOption, JSON.parse(readTextFileSync(prettierrc)))
+if (existsFileSync(prettierrc)) {
+    defaultOption = Object.assign(
+        defaultOption,
+        JSON.parse(readTextFileSync(prettierrc))
+    )
+}
 
 // Whether it was executed directly on the command line
 const CLI = wes.Modules[wes.main].path === __filename
